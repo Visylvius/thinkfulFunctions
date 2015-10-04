8e39b636fbf150c38d8d4f6b966d4d62e45c866a
Loren Barrick <lorenbarrick@gmail.com>
2015-10-03 18:42:15 -0700

    added lo-dash reduce, contains

---
+++ 2ndsaturdaysession.js
@@ -0,0 +1,71 @@
+var myArray = [1,2,3,4,5,6,7,8,9,10];
+function shuffle (cards) {
+  for (var i = 0; i < myArray.length; i++) {
+    Math.floor(Math.random(myArray[i]));
+  }
+  return cards;
+}
+
+
+
+
+function each (collection, callback) {
+  if (Array.isArray(collection)) {
+    for (var i = 0; i < collection.length; i++) {
+        callback(collection[i]);
+    } else if(typeof collection === 'object') {
+      for (var key in collection) {
+          callback(collection[key]);
+  }
+}
+return collection;
+}
+
+function even(num) {
+if (num % 2 === 0) {
+  return true;
+}
+}
+function some(collection, callback) {
+var result = false;
+each(collection, function(element) {
+    if(callback(element)) {
+       result = true;
+    }
+});
+return result;
+// if some of the items in this collection return true on the callback, then some returns true
+}
+  some(myArray, even);
+
+function every(collection, callback) {
+  var result = true;
+  each(collection, function(element) {
+    if(!callback(element)) {
+      result = false;
+    }
+  });
+  return result;
+// if all the items are true then return true on the callback
+}
+function reduce(collection, accumulator, startVal ) {
+var accumulatorVal = startVal;
+  each(collection, function(element) {
+    if(startVal === undefined) {
+      accumulatorVal = element;
+    }
+      accumulatorVal = accumulator(accumulatorVal, element);
+   });
+   return accumulatorVal;
+}
+
+
+
+
+function contains(collection, target) {
+  return reduce;
+
+  function add(a,b){
+  return a-b;
+}
+}
