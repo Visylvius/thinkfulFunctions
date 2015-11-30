a291774ed5b721db6bc0890dca77893ca1071f9d
Loren Barrick <lorenbarrick@gmail.com>
2015-11-29 17:36:25 -0800

    inital commit

---
+++ practiceInteview.js
@@ -0,0 +1,50 @@
+var numbers = [1,2,3,4,5,6,7,8,9,10];
+
+var letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n',
+               'o','p','q','r','s','t','u','v','w','x','y','z'];
+
+
+
+
+// Practice Problems:
+//-------------------
+
+function some() {
+
+}
+
+function every() {
+  
+}
+// Use the higherOrderFunctions to solve the following problems:
+
+// Use filter to return just the vowels from the array letters
+
+
+
+// Use each to print the odd numbers to the console
+
+
+
+// Use reduce to return the sum of numbers
+
+
+
+// Use map to convert the array letters to all capital letters
+
+
+// Use filter to return every other letter in letters
+
+
+
+// Modify the above function to return the other letters
+
+
+
+// Use filter to return the string "lmnop" from letters
+
+// HINT: more code here
+
+// Return the sum of the even numbers using filter and reduce together
+
+// NO HINTS: All of your code here
